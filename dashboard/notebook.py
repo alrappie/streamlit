22122d72b15ddb2e393b03f8144910b1b6fad1a0
Alrappie <alravimahesa@student.ub.ac.id>
2024-10-18 21:43:25 +0700

    update streamlit

--- dashboard/notebook.py
+++ dashboard/notebook.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Load cleaned data
 all_df = pd.read_csv("dashboard/used_data.csv")
@@ -17,6 +19,28 @@ most_profitable_product = all_df.groupby('product_category_name_english').agg({
  
 # plot number of daily orders (2021)
 st.header('Dicoding Collection Dashboard :sparkles:')
-st.subheader('Daily Orders')
 
-col1, col2 = st.columns(2)
+st.subheader("Best & Worst Performing Product")
+ 
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(35, 15))
+ 
+colors = ["#90CAF9", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+ 
+sns.barplot(x="quantity_x", y="product_name", data=most_selling_product.head(5), palette=colors, ax=ax[0])
+ax[0].set_ylabel(None)
+ax[0].set_xlabel("Number of Sales", fontsize=30)
+ax[0].set_title("Most Selling Product", loc="center", fontsize=50)
+ax[0].tick_params(axis='y', labelsize=35)
+ax[0].tick_params(axis='x', labelsize=30)
+ 
+sns.barplot(x="quantity_x", y="product_name", data=most_profitable_product.head(5), palette=colors, ax=ax[1])
+ax[1].set_ylabel(None)
+ax[1].set_xlabel("Number of Sales", fontsize=30)
+ax[1].invert_xaxis()
+ax[1].yaxis.set_label_position("right")
+ax[1].yaxis.tick_right()
+ax[1].set_title("Most Profitable Product", loc="center", fontsize=50)
+ax[1].tick_params(axis='y', labelsize=35)
+ax[1].tick_params(axis='x', labelsize=30)
+ 
+st.pyplot(fig)
