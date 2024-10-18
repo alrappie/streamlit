f3e117e4a8dbefedb08412ee348cdef94900fd3a
Alrappie <alravimahesa@student.ub.ac.id>
2024-10-19 01:12:26 +0700

    update

--- dashboard/notebook.py
+++ dashboard/notebook.py
@@ -9,7 +9,7 @@ all_df = pd.read_csv("dashboard/used_data.csv")
  
 st.header('Dicoding Collection Dashboard :sparkles:')
 
-col1, col2 = st.columns([.1,.9])
+col1, col2 = st.columns((2))
 
 with col1:
     city = st.selectbox(
@@ -23,40 +23,40 @@ with col2:
     else:
         df2 = all_df[all_df.customer_city=='city']
         
-        most_selling_product = df2.groupby('product_category_name_english').agg({
-            'product_id':'count',
-            'price':'sum'
-        }).sort_values('product_id',ascending=False)
+    most_selling_product = df2.groupby('product_category_name_english').agg({
+        'product_id':'count',
+        'price':'sum'
+    }).sort_values('product_id',ascending=False)
 
-        most_profitable_product = df2.groupby('product_category_name_english').agg({
-            'product_id':'count',
-            'price':'sum'
-        }).sort_values('price',ascending=False)
-        
-        # plot number of daily orders (2021)
+    most_profitable_product = df2.groupby('product_category_name_english').agg({
+        'product_id':'count',
+        'price':'sum'
+    }).sort_values('price',ascending=False)
+    
+    # plot number of daily orders (2021)
 
 
-        st.subheader("Top Selling & Profitable Product Category")
-        
-        fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(35, 15))
-        
-        colors = ["#90CAF9", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
-        
-        sns.barplot(x="product_id", y="product_category_name_english", data=most_selling_product.head(5).reset_index(), palette=colors, ax=ax[0])
-        ax[0].set_ylabel(None)
-        ax[0].set_xlabel("Number of Sales", fontsize=30)
-        ax[0].set_title("Most Selling Product", loc="center", fontsize=50)
-        ax[0].tick_params(axis='y', labelsize=35)
-        ax[0].tick_params(axis='x', labelsize=30)
-        
-        sns.barplot(x="price", y="product_category_name_english", data=most_profitable_product.head(5).reset_index(), palette=colors, ax=ax[1])
-        ax[1].set_ylabel(None)
-        ax[1].set_xlabel("Number of Sales (in million)", fontsize=30)
-        ax[1].invert_xaxis()
-        ax[1].yaxis.set_label_position("right")
-        ax[1].yaxis.tick_right()
-        ax[1].set_title("Most Profitable Product", loc="center", fontsize=50)
-        ax[1].tick_params(axis='y', labelsize=35)
-        ax[1].tick_params(axis='x', labelsize=30)
-        
-        st.pyplot(fig)
+    st.subheader("Top Selling & Profitable Product Category")
+    
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(35, 15))
+    
+    colors = ["#90CAF9", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+    
+    sns.barplot(x="product_id", y="product_category_name_english", data=most_selling_product.head(5).reset_index(), palette=colors, ax=ax[0])
+    ax[0].set_ylabel(None)
+    ax[0].set_xlabel("Number of Sales", fontsize=30)
+    ax[0].set_title("Most Selling Product", loc="center", fontsize=50)
+    ax[0].tick_params(axis='y', labelsize=35)
+    ax[0].tick_params(axis='x', labelsize=30)
+    
+    sns.barplot(x="price", y="product_category_name_english", data=most_profitable_product.head(5).reset_index(), palette=colors, ax=ax[1])
+    ax[1].set_ylabel(None)
+    ax[1].set_xlabel("Number of Sales (in million)", fontsize=30)
+    ax[1].invert_xaxis()
+    ax[1].yaxis.set_label_position("right")
+    ax[1].yaxis.tick_right()
+    ax[1].set_title("Most Profitable Product", loc="center", fontsize=50)
+    ax[1].tick_params(axis='y', labelsize=35)
+    ax[1].tick_params(axis='x', labelsize=30)
+    
+    st.pyplot(fig)
